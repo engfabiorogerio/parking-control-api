7fc47dfc4124c964b2a1912d5dda9d1f78e23627
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-09-12 12:42:07 -0300

    Initial Commit

---
+++ projeto-springboot-restapi2/src/test/java/com/engfabiorogerio/projetospringbootrest2/ProjetoSpringbootRestapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engfabiorogerio.projetospringbootrest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootRestapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
